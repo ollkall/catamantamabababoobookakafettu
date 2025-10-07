1b60ec4d0ad0512b43009c69982c73b244ac3d24
Oliver Kallassalu <ollkall@users.noreply.github.com>
2025-10-07 13:24:30 +0300

    Lisatud 07.10 tehtud tunnitöö: andmeühendus, andmete loomine ja kirja juhtimine

--- indeks.js
+++ indeks.js
@@ -2,6 +2,10 @@ const express = require("express");
 const dateET = require("./src/dateTimeET");
 const fs = require("fs");
 const bodyparser = require("body-parser");
+//lisan andmebaasiga suhtlemiseks mooduli
+const mysql = require("mysql2");
+//Lisan andmebaasi juurdepääsuinfo
+const dbInfo = require("../../../oliver2025config.js");
 
 // me loome objekti, mis ongi epxress.js programm ja edasi kasutamegi seda
 
@@ -15,6 +19,14 @@ app.use(express.static("public"));
 // päringu URL-i parsimine, eraldame POST osa. False, kui ainult tekst, true, kui ainult infot Ka
 
 app.use(bodyparser.urlencoded({extended: false}));
+
+//loon andmebaasiühenduse
+const conn = mysql.createConnection({
+	host: dbInfo.configdata.host,
+	user: dbInfo.configdata.user,
+	password: dbInfo.configdata.passWord,
+	database: dbInfo.configdata.dataBase
+});
 
 app.get("/", (req, res)=>{
 	//res.send("Express.js läks edukalt käima!!!");
@@ -40,7 +52,7 @@ app.get("/reqvisit", (req, res)=>{
 	res.render("reqvisit");
 });
 
-app.post("/reqvisit", (req, res)=>{
+app.post("/regvisit", (req, res)=>{
 	console.log(req.body);
 	//avan tekstifaili kirjutamiseks sellisel moel, et kui teda pole, luuakse (parameeter "a")
 	fs.open("public/txt/visitlog.txt", "a", (err, file)=>{
@@ -49,17 +61,81 @@ app.post("/reqvisit", (req, res)=>{
 		}
 		else {
 			//faili senisele sisule lisamine
-			fs.appendFile("public/txt/visitlog.txt", req.body.nameInput + "; ", (err)=>{
+			fs.appendFile("public/txt/visitlog.txt", req.body.firstNameInput + " " + req.body.lastNameInput + ", " + dateEt.longDate() + " kell " + dateEt.time() + ";", (err)=>{
 				if(err){
 					throw(err);
 				}
 				else {
 					console.log("Salvestatud!");
-					res.render("reqvisit");
+					res.render("visitregistered", {visitor: req.body.firstNameInput + " " + req.body.lastNameInput});
 				}
 			});
 		}
 	});
+});
+
+app.get("/visitlog", (req, res)=>{
+	let listData = [];
+	fs.readFile("public/txt/visitlog.txt", "utf8", (err, data)=>{
+		if(err){
+			//kui tuleb viga, siis ikka vأ¤ljastame veebilehe, liuhtsalt vanasأµnu pole أ¼htegi
+			res.render("genericlist", {heading: "Registreeritud külastused", listData: ["Ei leidnud ühtegi külastust!"]});
+		}
+		else {
+			let tempListData = data.split(";");
+			for(let i = 0; i < tempListData.length - 1; i ++){
+				listData.push(tempListData[I]);
+			}
+			res.render("genericlist", {heading: "Registreeritud külastused", listData: listData});
+		}
+	});
+});
+
+app.get("/eestifilm", (req, res)=>{
+	res.render("eestifilm");
+});
+
+app.get("/eestifilm/filmiinimesed", (req, res)=>{
+	const sqlReq = "SELECT * FROM person";
+	conn.execute(sqlReq, (err, sqlRes)=>{
+		if(err){
+			console.log(err);
+			res.render("filmiinimesed", {personList: []});
+		}
+		else {
+			console.log(sqlRes);
+			res.render("filmiinimesed", {personList: sqlRes});
+		}
+	})
+	//res.render("filmiinimesed");
+});
+
+app.get("/eestifilm/filmiinimesed_add", (req, res)=>{
+	res.render("filmiinimesed_add", {notice: "Ootan sisestust!"});
+});
+
+app.post("/eestifilm/filmiinimesed_add", (req, res)=>{
+	console.log(req.body);
+	// Kas andmed on olemas?
+	if(!req.body.firstNameInput || !req.body.lastNameInput || !req.body.bornInput || req.body.bornInput > new Date()){
+		res.render("filmiinimesed_add", {notice: "Andmed on vigased! Vaata üle!" + req.body});
+	}
+	else {
+		let deceasedDate = null;
+		if(req.body.deceasedInput != ""){
+			deceasedDate = req.body.deceasedInput
+		}
+		let sqlReq = "INSERT INTO person (first_name, last_name, born, deceased) VALUES (?,?,?,?)";
+		conn.execute(sqlReq, [req.body.firstNameInput, req.body.lastNameInput, req.body.bornInput, deceasedDate], (err, sqlRes)=>{
+			if(err){
+				res.render("filmiinimesed_add", {notice: "Tekkis tehniline viga:" + err});
+			}
+			else {
+				res.render("filmiinimesed_add", {notice: "Andmed on salvestatud!"});
+			}
+		});
+	}
+	//res.render("filmiinimesed_add", {notice: "Andmed olemas!" + req.body});
 });
 
 app.listen(5204);
